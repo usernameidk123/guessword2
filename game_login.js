10485f2f015fae3d7bc001d278fc126e14163b22
usernameidk123 <87160461+usernameidk123@users.noreply.github.com>
2022-02-15 18:35:15 -0800

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,9 @@
+function adduser(){
+    player1_name=document.getElementById("player1_name_input").value;
+    player2_name=document.getElementById("player2_name_input").value;
+
+    localStorage.setItem("player1",player1_name);
+    localStorage.setItem("player2",player2_name);
+
+    window.location.replace("game_page.html");
+}
